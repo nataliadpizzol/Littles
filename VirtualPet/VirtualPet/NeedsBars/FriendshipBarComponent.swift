e073486a4d530bb0afa9a980ff77596e2278dfb6
Natalia Dal Pizzol <nataliadpizzol@gmail.com>
2023-11-06 15:30:57 -0300

    feat: bar components

---
+++ VirtualPet/VirtualPet/NeedsBars/FriendshipBarComponent.swift
@@ -0,0 +1,37 @@
+//
+//  FriendshipBarComponent.swift
+//  VirtualPet
+//
+//  Created by Natalia Dal Pizzol on 27/10/23.
+//
+
+import SwiftUI
+
+struct FriendshipBarComponent: View {
+    @State private var progress: Float = 0.0
+        
+    var body: some View {
+        VStack {
+            ProgressView("Loading", value: progress, total: 1.0)
+                .progressViewStyle(
+                    LinearProgressViewStyle(tint: .background))
+                .frame(width: 200)
+                .padding()
+        }
+        
+        Button("Increase Friendship") {
+            if progress < 1.0 {
+                progress += 0.1
+            }
+        }
+        .buttonPrimary(isDisabled: false)
+        .padding()
+        Button("Decrease Friendship") {
+            if progress > 0.0 {
+                progress -= 0.1
+            }
+        }
+        .buttonPrimary(isDisabled: false)
+        .padding()
+    }
+}
